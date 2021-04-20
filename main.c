7b3ced8615efbd692d220af8ddb8edeeb3dc8531
Minsung Kim <minsungk@c7r2s7.42seoul.kr>
2021-04-20 16:41:02 +0900

    fix header

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+<<<<<<< HEAD
 #include "ft_printf.h"
 
 int main()
@@ -6,4 +7,12 @@ int main()
 	int i = 198;
 	printf("-->|%-0.10d|<--\n", 198);
 	ft_printf("-->|%-0.10i|<--\n", i);
+=======
+#include "ft_printf.c"
+
+int main()
+{
+	printf("%c\n", 'a');
+	ft_printf("--->|%c|<---- ", 'a');
+>>>>>>> 72c2209... fix header
 }
